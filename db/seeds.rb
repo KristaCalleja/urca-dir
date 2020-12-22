ea5e30319b3f9d6ec6f72996f5f7a00154ff3245
KristaCalleja <kristacm@gmail.com>
2020-12-22 16:19:35 -0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Contact.destroy_all
+puts "Creating contacts in 3... 2... 1..."
+first_contact = Contact.new(
+    name: "n/a", 
+    surname: "n/a", 
+    profession: "n/a", 
+    shop: "Papelaria Urca", 
+    category: "papelaria", 
+    number: "97358-6375", 
+    location: true
+)
+first_contact.save!
+
+second_contact = Contact.new(
+    name: "Leandro", 
+    surname: "n/a", 
+    profession: "Bombeiro", 
+    shop: "Hidraulico", 
+    category: "hidraulico", 
+    number: "97456-3907", 
+    location: false
+)
+second_contact.save!
+
+third_contact = Contact.new(
+    name: "Josemi", 
+    surname: "n/a", 
+    profession: "Técnico", 
+    shop: "n/a", 
+    category: "conserta maquina de lavar", 
+    number: "99977-2511", 
+    location: false
+)
+puts "Finish seeding."
